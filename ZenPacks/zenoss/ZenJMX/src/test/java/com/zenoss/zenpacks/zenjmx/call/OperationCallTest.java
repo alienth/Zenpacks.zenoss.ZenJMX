f3a3d6bb71d0b8de188f30fcd30ea1f7532a1478
chris <chris@4f473830-b6da-0310-973f-bbbfb169328c>
2008-06-05 21:42:39 +0000

    * added
    * these are some unit tests that verify hashCode() and equals() work
    
    
    git-svn-id: http://dev.zenoss.org/svn/trunk/zenpacks/ZenPacks.zenoss.ZenJMX@9455 4f473830-b6da-0310-973f-bbbfb169328c

---
+++ ZenPacks/zenoss/ZenJMX/src/test/java/com/zenoss/zenpacks/zenjmx/call/OperationCallTest.java
@@ -0,0 +1,138 @@
+package com.zenoss.zenpacks.zenjmx.call;
+
+import junit.framework.*;
+import junit.textui.TestRunner;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+/**
+ * <p> Tests the methods in the OperationAttributeCall class.  Don't
+ * feel reassured though - the extensiveness of the test is
+ * lacking...  </p>
+ *
+ * <p>$Author: chris $</p>
+ *
+ * @author Christopher Blunck
+ * @version $Revision: 1.6 $
+ */
+public class OperationCallTest
+  extends TestCase {
+
+  // the configuration for the call
+  private static final String URL = 
+    "service:jmx:jmxrmi://localhost:12345/mbean";
+  private static final boolean AUTHENTICATE = true;
+  private static final String USERNAME = "username";
+  private static final String PASSWORD = "password";
+  private static final String OBJECT_NAME = "mbean";
+  private static final String ATTR_NAME = "attribute";
+  private static final String ATTR_TYPE = "attribute type";
+  private static Object[] PARAM_VALUES;
+  private static String[] PARAM_TYPES;
+  private static List<String> KEYS;
+  private static List<String> TYPES;
+
+  // the calls we will test
+  private static OperationCall _call1;
+  private static OperationCall _call2;
+
+
+  // staticly initialize the arrays and lists we will pass to the calls
+  static {
+    PARAM_VALUES = new Object[] {
+      "param1", "param2"
+    };
+
+    PARAM_TYPES = new String[] {
+      "java.lang.String", "java.lang.String"
+    };
+
+    KEYS = new ArrayList<String>();
+    KEYS.add("key1");
+    KEYS.add("key2");
+
+    TYPES = new ArrayList<String>();
+    TYPES.add("java.lang.String");
+    TYPES.add("java.lang.Double");
+  }
+
+
+  /**
+   * Constructs a test that invokes a specific test method
+   */
+  public OperationCallTest(String method) {
+    super(method);
+  }
+
+
+  /**
+   * Called before each method is invoked.  Sets up the two call
+   * objects we will use.
+   */
+  public void setUp() { 
+    _call1 = 
+      new OperationCall(URL, AUTHENTICATE, 
+                        USERNAME, PASSWORD, 
+                        OBJECT_NAME, ATTR_NAME,
+                        PARAM_VALUES,
+                        PARAM_TYPES,
+                        KEYS, TYPES);
+
+    _call2 = 
+      new OperationCall(URL, AUTHENTICATE, 
+                        USERNAME, PASSWORD, 
+                        OBJECT_NAME, ATTR_NAME,
+                        PARAM_VALUES,
+                        PARAM_TYPES,
+                        KEYS, TYPES);
+  }
+
+
+  /**
+   * Called after each method finishes execution
+   */
+  public void tearDown() { 
+    _call1 = null;
+    _call2 = null;
+  }
+
+
+  /**
+   * Defines the list of test methods to run.  By default we'll run
+   * 'em all.
+   */
+  public static Test suite() {
+    TestSuite suite = new TestSuite(OperationCallTest.class);
+    return suite;
+  }
+
+  
+  /**
+   * Runs all the tests via the command line.
+   */
+  public static void main(String[] args) {
+    TestRunner.run(OperationCallTest.class);
+  }
+
+
+  /*
+   * TEST METHODS BEGIN HERE
+   */
+
+  /**
+   * Tests the hashCode() method
+   */
+  public void testHashCode() {
+    assertEquals(_call1.hashCode(), _call2.hashCode());
+  }
+
+
+  /**
+   * Tests the equals() method
+   */
+  public void testEquals() {
+    assertTrue(_call1.equals(_call2));
+  }
+}
