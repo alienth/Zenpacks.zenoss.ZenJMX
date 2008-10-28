3214fc63b58ed6ac83e26969832e4e1e39cad0e1
jplouis <jplouis@4f473830-b6da-0310-973f-bbbfb169328c>
2008-10-28 17:21:43 +0000

    updating licenses for java code
    git-svn-id: http://dev.zenoss.org/svn/trunk/zenpacks/ZenPacks.zenoss.ZenJMX@10810 4f473830-b6da-0310-973f-bbbfb169328c

--- ZenPacks/zenoss/ZenJMX/src/main/java/com/zenoss/zenpacks/zenjmx/call/MultiValueAttributeCall.java
+++ ZenPacks/zenoss/ZenJMX/src/main/java/com/zenoss/zenpacks/zenjmx/call/MultiValueAttributeCall.java
@@ -1,13 +1,15 @@
 ///////////////////////////////////////////////////////////////////////////
 //
-// This program is part of Zenoss Core, an open source monitoring platform.
-// Copyright (C) 2008, Zenoss Inc.
-//
-// This program is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License version 2 as published by
-// the Free Software Foundation.
-//
-// For complete information please visit: http://www.zenoss.com/oss/
+//Copyright 2008 Zenoss Inc 
+//Licensed under the Apache License, Version 2.0 (the "License"); 
+//you may not use this file except in compliance with the License. 
+//You may obtain a copy of the License at 
+//    http://www.apache.org/licenses/LICENSE-2.0 
+//Unless required by applicable law or agreed to in writing, software 
+//distributed under the License is distributed on an "AS IS" BASIS, 
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+//See the License for the specific language governing permissions and 
+//limitations under the License.
 //
 ///////////////////////////////////////////////////////////////////////////
 package com.zenoss.zenpacks.zenjmx.call;
