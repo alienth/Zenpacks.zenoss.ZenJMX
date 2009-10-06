2019db552ade205ed473779b8f72cdf1c8928e44
bedwards <bedwards@4f473830-b6da-0310-973f-bbbfb169328c>
2009-10-06 15:01:37 +0000

    internal tickets 1935, 1963, 1967. merge bedwards/1935c 15317-15323. fix upgrade for enterprise zenpacks. reviewed by Chip.
    git-svn-id: http://dev.zenoss.org/svn/trunk/zenpacks/ZenPacks.zenoss.ZenJMX@15327 4f473830-b6da-0310-973f-bbbfb169328c

---
+++ ZenPacks/zenoss/ZenJMX/migrate/MigratePassword.py
@@ -0,0 +1,29 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2009, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+
+import logging
+log = logging.getLogger("zen.migrate")
+
+import Globals
+from Products.ZenModel.migrate.Migrate import Version
+from Products.ZenModel.ZenPack import ZenPackMigration
+from Products.ZenModel.migrate.MigrateUtils import migratePropertyType
+
+class MigratePassword(ZenPackMigration):
+    version = Version(3, 3, 0)
+
+    def migrate(self, dmd):
+        log.info("Migrating zJmxPassword")
+        migratePropertyType("zJmxPassword", dmd, "string")
+        
+MigratePassword()
